184c1b728410e85a6d35e508e0ec6651514653e6
Barrydian <dianbarrysaram@yahoo.fr>
2021-06-14 11:15:42 +0200

    Ajusting Epoch

--- constants.py
+++ constants.py
@@ -12,14 +12,13 @@ slim = tf.contrib.slim
 
 HEIGHT, WIDTH, CHANNEL = 128, 128, 3
 BATCH_SIZE = 64
-EPOCH = 5000
+EPOCH = 10000
 version = 'newOctImages'
 newOCT_path = './' + version
 
 # number couche convele generator 
 nb_couch_gen4, nb_couch_gen8, nb_couch_gen16, nb_couch_gen32, nb_couch_gen64 = 512, 256, 128, 64, 32 # nb_couch_genhannel num
 s4 = 4
-
 
 # channnel nm discriminator 
 ch_disc2, ch_disc4, ch_disc8, ch_disc16 = 64, 128, 256, 512  # channel num: 64, 128, 256, 512
